b29011db6dc58d19e9553199a87b12266fe0e783
YASSINE-AA <overflowtn@gmail.com>
2025-02-06 20:46:45 +0100

    got rid of wgl_init

--- include/glps_wgl_context.h
+++ include/glps_wgl_context.h
@@ -2,7 +2,6 @@
 #define GLPS_WGL_CONTEXT_H
 #include <glps_common.h>
 
-void glps_wgl_init(glps_WindowManager *wm);
 void glps_wgl_make_ctx_current(glps_WindowManager *wm, size_t window_id);
 void *glps_wgl_get_proc_addr();
 void glps_wgl_swap_buffers(glps_WindowManager *wm, size_t window_id);
--- src/glps_wgl_context.c
+++ src/glps_wgl_context.c
@@ -1,7 +1,5 @@
 #include <glps_wgl_context.h>
 
-void glps_wgl_init(glps_WindowManager *wm) {}
-
 
 void glps_wgl_make_ctx_current(glps_WindowManager *wm, size_t window_id) {
   wglMakeCurrent(wm->windows[window_id]->hdc, wm->win32_ctx->hglrc);
